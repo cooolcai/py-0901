79dd5044efbb5f10d864ff2a2df00b58c15e6f28
czm@local.lan <915734072@qq.com>
2020-12-19 14:58:43 +0800

    Create network_bandwidth.py

---
+++ 06-变量定义/network_bandwidth.py
@@ -0,0 +1,19 @@
+# 网络带宽计算
+#print(100/8)本行为逻辑。
+#变量：用等号来进行赋值，把后边的值赋给前边。
+bandwidth = 100
+ratio = 8
+
+print(bandwidth/ratio)
+#
+
+
+
+#注意点
+#变量值可以多次赋值。
+#a=3 b=a b=3
+
+#变量的命名需要注意：多种方法风格
+# bandWidth
+# BandWidth
+# band_width
